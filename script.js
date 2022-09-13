a4fee5c9d1673d691fd6d628eba6f83618b4b5b4
Masoud Nazarii <masoudnazari.dev@gmail.com>
2022-09-13 20:22:03 +0430

    add style trying to add dark mode

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+//style
+const style = document.createElement('style');
+style.innerHTML = `
+      .city {
+        font-weight: lighter;
+      }
+    `;
+document.head.appendChild(style);
+
+
+
 let weather = {
     fetchWeather: function (city) {
         fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + config.apiKey)
@@ -10,30 +21,55 @@ let weather = {
         const {icon, description} = data.weather[0]
         const {temp, feels_like, humidity} = data.main
         const {speed} = data.wind
-
-        document.querySelector(".city").innerText = "Weather in " + name;
+        
         document.querySelector(".icon").src ="https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".city").innerText = "Weather in " + name;
         document.querySelector(".description").innerText = description;
         document.querySelector(".temp").innerText = Math.round(temp) + "°C";
-        document.querySelector(".feels_like").innerText = "Feels like " + Math.round(feels_like) + " °C";
+        document.querySelector(".feels_like").innerText = "Feels like " + Math.round(feels_like) + "°C";
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
-        document.querySelector(".wind").innerText = "Wind speed: " + Math.round(speed) + " km/h";
+        document.querySelector(".wind").innerText = "Wind speed: " + Math.round(speed) + "km/h";
 
+        
+        
         document.querySelector(".weather").classList.remove("loading")
     },
     search: function () {
         this.fetchWeather(document.querySelector(".search-bar").value);
-    }
+    },
+    /* 
+    skycon: function setIcons(data, iconID) {
+        const {description} = data.weather[0]
+        const skycons = new Skycons({"color": "white"});
+        const currentIcon = description.replace(/ /g, "_").toUpperCase();
+        skycons.play();
+        skycons.set(iconID, Skycons[currentIcon]);
+        skycons.add(document.getElementById(".icon"));
+    },
+    */
 };
-        document.querySelector(".search button")
-        .addEventListener("click", function () {
-            weather.search();
-        } );
 
-        document.querySelector(".search-bar")
-        .addEventListener("keyup", function (event) {
-            if(event.key == "Enter") {
-                weather.search();
-            }
-        });
-        
+document.querySelector(".search button").addEventListener("click", function () {
+    weather.search();
+});
+
+document.querySelector(".search-bar").addEventListener("keyup", function (event) {
+    if(event.key == "Enter") {
+        weather.search();
+    }
+});
+
+const chk = document.getElementById("chk");
+const card = document.getElementById("my-card");
+
+chk.addEventListener("change", () => {
+	card.classList.add.toggle("dark");
+});
+
+
+
+
+
+
+
+
