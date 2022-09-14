ea32c7c2ee5cbbc34b491f5802c5a12eb5ca8b27
Masoud Nazarii <masoudnazari.dev@gmail.com>
2022-09-14 17:05:42 +0430

    fix theme

--- script.js
+++ script.js
@@ -59,11 +59,31 @@ document.querySelector(".search-bar").addEventListener("keyup", function (event)
     }
 });
 
-const chk = document.getElementById("chk");
-const card = document.getElementById("my-card");
 
-chk.addEventListener("change", () => {
-	card.classList.add.toggle("dark");
+// Change Theme
+const checkbox = document.querySelector(".checkbox");
+const gradient = document.querySelector(".gradient");
+const label = document.querySelector(".label");
+const moon = document.querySelector(".moon");
+const sun = document.querySelector(".sun");
+const card = document.querySelector(".card");
+const searchButton = document.querySelector(".search-button");
+const searchBar = document.querySelector(".search-bar");
+const searchBarHover = document.querySelector(".search-button");
+const loading = document.querySelector(".loading");
+const placeholder = document.querySelector(".search-bar");
+
+checkbox.addEventListener("click", function() {
+    gradient.classList.toggle("gradient-dark");
+    label.classList.toggle("label-dark");
+    moon.classList.toggle("moon-sun-dark");
+    sun.classList.toggle("moon-sun-dark");
+	card.classList.toggle("card-dark");
+    searchButton.classList.toggle("search-button-dark");
+    searchBar.classList.toggle("search-bar-dark");
+    searchBarHover.classList.toggle("search-hover-dark");
+    loading.classList.toggle("loading-dark");
+    placeholder.classList.toggle("placeholder-dark");
 });
 
 
