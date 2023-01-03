1141c8ea341130278061d362d308d543e78f483c
Masoud Nazarii <masoudnazari.dev@gmail.com>
2023-01-03 20:06:11 +0330

    Animationed icons

--- js/script.js
+++ js/script.js
@@ -23,8 +23,9 @@ let weather = {
         
         const Sunrise = new Date(sunrise);
         const Sunset = new Date(sunset);
-
-        document.querySelector(".icon").src ="https://openweathermap.org/img/wn/" + icon + ".png";
+        
+        
+        document.querySelector(".icon").src ="../icons/weather-icons-master/production/line/openweathermap/" + icon + ".svg";
         document.querySelector(".city").innerText = "Weather in " + name;
         document.querySelector(".description").innerText = description;
         document.querySelector(".temp").innerText = Math.round(temp) + "°C";
@@ -38,7 +39,7 @@ let weather = {
     },
     search: function () {
         this.fetchWeather(document.querySelector(".search-bar").value);
-    },    
+    }, 
 };
 
 document.querySelector(".search button").addEventListener("click", function () {
@@ -50,14 +51,3 @@ document.querySelector(".search-bar").addEventListener("keyup", function (event)
         weather.search();
     }
 });
-
-/* 
-    skycon: function setIcons(data, iconID) {
-        const {description} = data.weather[0]
-        const skycons = new Skycons({"color": "white"});
-        const currentIcon = description.replace(/ /g, "_").toUpperCase();
-        skycons.play();
-        skycons.set(iconID, Skycons[currentIcon]);
-        skycons.add(document.getElementById(".icon"));
-    },
-*/
