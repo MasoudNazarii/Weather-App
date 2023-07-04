554c75db825e7af577be0111a945b98db6f971c3
Masoud Nazarii <masoudnazari.dev@gmail.com>
2023-07-04 17:45:33 +0330

    first changes

---
+++ weather/src/App.js
@@ -0,0 +1,14 @@
+import './styles/App';
+import Navbar from './Components/Navbar';
+import MainCard from './Components/MainCard';
+
+function App() {
+  return (
+    <>
+		<Navbar />
+		<MainCard />
+    </>
+  );
+}
+
+export default App;
