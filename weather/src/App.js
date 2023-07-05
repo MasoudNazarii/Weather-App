fd192914898d3e602f76372cd7a9d59f6fd7eb89
Masoud Nazarii <masoudnazari.dev@gmail.com>
2023-07-05 21:18:47 +0330

    fix bugs and add bootstrap

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,4 +1,6 @@
-import './styles/App';
+import 'bootstrap/dist/css/bootstrap.css';
+import './styles/App.css';
+import "bootstrap/dist/js/bootstrap.bundle.min";
 import Navbar from './Components/Navbar';
 import MainCard from './Components/MainCard';
 
