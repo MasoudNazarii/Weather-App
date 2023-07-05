26f7e53086a7f718fb30da84a4345c048dc904be
Masoud Nazarii <masoudnazari.dev@gmail.com>
2023-07-05 21:15:49 +0330

    add new things

--- weather/src/Components/Weather.js
+++ weather/src/Components/Weather.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import { Drop } from "./Icons";
+import { Wind } from "./Icons";
+// import Search from "./Search";
+// import Axios from 'axios';
+// import config from './APIKey';
 
 
 export default function Weather() {
+    // const [result, setResult] = useState({});
+
+    // const fetchAPI=() =>{
+    //     Axios.get(
+    //         `https://api.openweathermap.org/data/2.5/weather?q=$(Search.cityName)&units=metric&appid=$(config.apiKey)`
+    //         ).then((Response)=>{
+    //         setResult(Response.data)
+    //     })
+    // }
+
     return(
         <>
             <div className="weather loading">
@@ -12,47 +27,14 @@ export default function Weather() {
                 <div className="description" />
                 <div className="div-table">
                     <div className="div-row">
-                    <div className="humidity" />
-                    <svg
-                        className="drop-icon"
-                        stroke="currentColor"
-                        fill="currentColor"
-                        strokeWidth={0}
-                        viewBox="0 0 16 16"
-                        height="1.2em"
-                        width="1.2em"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path
-                        fillRule="evenodd"
-                        d="M7.21.8C7.69.295 8 0 8 0c.109.363.234.708.371 1.038.812 1.946 2.073 3.35 3.197 4.6C12.878 7.096 14 8.345 14 10a6 6 0 01-12 0C2 6.668 5.58 2.517 7.21.8zm.413 1.021A31.25 31.25 0 005.794 3.99c-.726.95-1.436 2.008-1.96 3.07C3.304 8.133 3 9.138 3 10a5 5 0 0010 0c0-1.201-.796-2.157-2.181-3.7l-.03-.032C9.75 5.11 8.5 3.72 7.623 1.82z"
-                        clipRule="evenodd"
-                        />
-                        <path
-                        fillRule="evenodd"
-                        d="M4.553 7.776c.82-1.641 1.717-2.753 2.093-3.13l.708.708c-.29.29-1.128 1.311-1.907 2.87l-.894-.448z"
-                        clipRule="evenodd"
-                        />
-                    </svg>
+                        <div className="humidity" />
+                        <Drop />
                     </div>
                 </div>
                 <div className="div-table">
                     <div className="div-row">
-                    <div className="wind" />
-                    <svg
-                        className="wind-icon"
-                        stroke="currentColor"
-                        fill="none"
-                        strokeWidth={2}
-                        viewBox="0 0 24 24"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        height="1.2em"
-                        width="1.2em"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path d="M9.59 4.59A2 2 0 1 1 11 8H2m10.59 11.41A2 2 0 1 0 14 16H2m15.73-8.27A2.5 2.5 0 1 1 19.5 12H2" />
-                    </svg>
+                        <div className="wind" />
+                        <Wind />
                     </div>
                 </div>
                 <p>
