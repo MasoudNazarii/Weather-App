9905c04663817c63d85efdde1d1f7f38d92cf73b
Masoud Nazarii <masoudnazari.dev@gmail.com>
2023-07-08 21:07:47 +0330

    test theme

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,4 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.css';
+// import './theme.js';
+// import './styles/theme.css'
 import './styles/App.css';
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import Navbar from './Components/Navbar';
