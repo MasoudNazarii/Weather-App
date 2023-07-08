9c4f7b87e0499d8b6058d25c3332ebbdb7c266f6
Masoud Nazarii <masoudnazari.dev@gmail.com>
2023-07-08 21:10:30 +0330

    remove search component

--- weather/src/Components/Search.js
+++ weather/src/Components/Search.js
@@ -1,23 +1,25 @@
-import React, { useState } from 'react';
-import { SearchIco } from './Icons';
+// import React, { useState } from 'react';
+// import { SearchIco } from './Icons';
 // import Weather from './Weather';
 
-export default function Search() {
-    // const [cityName, setCityName] = useState("");
+
+// export default function Search() {
+//     const [cityName, setCityName] = useState("");
 
 
-    return(
-        <>
-            <div className="search">
-                <input type="text" 
-                        className="search-bar" 
-                        placeholder="Search" 
-                        // onChange={(event)=> setCityName(event.target.value)}
-                />
-                <button className="search-button">
-                    <SearchIco />
-                </button>
-            </div>
-        </>
-    );
-}
+//     return(
+//         <>
+//             <div className="search">
+//                 <input type="text" 
+//                         className="search-bar" 
+//                         placeholder="Search" 
+//                         onChange={(event)=> setCityName(event.target.value)}
+//                 />
+//                 <button className="search-button"
+//                         onClick={fetchAPI}>
+//                     <SearchIco />
+//                 </button>
+//             </div>
+//         </>
+//     );
+// }
