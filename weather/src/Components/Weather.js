2c578287faec6fdc4f57302aa1d436a4b9de0532
Masoud Nazarii <masoudnazari.dev@gmail.com>
2023-07-09 21:10:28 +0330

    fix bugs

--- weather/src/Components/Weather.js
+++ weather/src/Components/Weather.js
@@ -3,48 +3,56 @@ import { Drop, Wind, SearchIco } from "./Icons";
 import Axios from 'axios';
 import config from './APIKey';
 
-
 export default function Weather() {
     const [result, setResult] = useState({});
     const [cityName, setCityName] = useState("");
-
+    const [show, setShow] = useState(false);
+    
     const fetchAPI=() =>{
         Axios.get(
             `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${config.apiKey}`
             ).then((Response)=>{
-            setResult(Response.data)
-            console.log(cityName);
-        })
+                setResult(Response.data)
+            })
+        console.log(cityName);
+        console.log(result);
+        setShow(true);
+        
     }
-
-    return(
-        <>
+    const handelKeyDown = (event) => {
+        if (event.key === 'Enter') {
+            fetchAPI();
+        }
+    }
+        return(
+            <>
             <div className="search">
                 <input type="text" 
                         className="search-bar" 
                         placeholder="Search" 
                         onChange={(event)=> setCityName(event.target.value)}
+                        onKeyDown={handelKeyDown}
                 />
                 <button className="search-button"
                         onClick={fetchAPI}>
                     <SearchIco />
                 </button>
             </div>
-            <div className="weather loading">
-                <h1 className="city" />
-                <div className="temp" />
-                <div className="feels_like" />
-                <img src="" alt="" className="icon" />
-                <div className="description" />
+            <div className={show? "weather" : "weather loading"}>
+                <h1 className="city">Weather in {result?.name}</h1>
+                <div className="temp">{Math.round(result?.main?.temp)}°C</div>
+1               <div className="feels_like">Feels like {Math.round(result?.main?.feels_like)}°C</div>
+                <img src="" alt="" className="icon"/>
+                <div className="description">{result?.weather?.description}</div>
                 <div className="div-table">
                     <div className="div-row">
-                        <div className="humidity" />
+                        <div className="humidity">Humidity: {result?.main?.humidity}%</div>
                         <Drop />
                     </div>
                 </div>
                 <div className="div-table">
                     <div className="div-row">
-                        <div className="wind" />
+                        <div className="wind">Wind speed: {Math.round(result?.wind?.speed)}km/h</div>
                         <Wind />
                     </div>
                 </div>
@@ -62,9 +70,9 @@ export default function Weather() {
                 </p>
                 <div className="collapse" id="collapseExample">
                     <div className="card card-body">
-                    <div className="sunrise" />
-                    <div className="sunset" />
-                    <div className="date" />
+                    <div className="sunrise">Sunrise time: {result?.sys?.sunrise}</div>
+                    <div className="sunset">Sunset time: {result?.sys?.sunset}</div>
+                    <div className="date">{result?.timezone}</div>
                     </div>
                 </div>
             </div>
