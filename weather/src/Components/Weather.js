aea95f96fe6ddf8ba69c5ef5b16d19bdcce14889
Masoud Nazarii <masoudnazari.dev@gmail.com>
2023-07-10 21:44:04 +0330

    fix time and some bugs

--- weather/src/Components/Weather.js
+++ weather/src/Components/Weather.js
@@ -2,28 +2,40 @@ import React, { useState } from "react";
 import { Drop, Wind, SearchIco } from "./Icons";
 import Axios from 'axios';
 import config from './APIKey';
+import WeatherIcons from "../icons/weather-icons-master/production/line/openweathermap/index.js";
 
 export default function Weather() {
     const [result, setResult] = useState({});
     const [cityName, setCityName] = useState("");
     const [show, setShow] = useState(false);
+    const [icon, setIcon] = useState("");
+    const [description, setDescription] = useState('');
+    
     
     const fetchAPI=() =>{
         Axios.get(
             `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${config.apiKey}`
             ).then((Response)=>{
                 setResult(Response.data)
+                setShow(true);
+                setIcon(Response.data['weather'][0]['icon']);
+                setDescription(Response.data['weather'][0]['description']);
+                console.log(icon);
+                console.log(description);
             })
-        console.log(cityName);
-        console.log(result);
-        setShow(true);
         
     }
+    
     const handelKeyDown = (event) => {
         if (event.key === 'Enter') {
             fetchAPI();
         }
     }
+    const sunriseTime = new Date(result?.sys?.sunrise * 1000).toLocaleTimeString();
+    const sunsetTime = new Date(result?.sys?.sunset * 1000).toLocaleTimeString();
+
+    const iconFilename = icon ? `wi-${icon}.svg` : '';
+
         return(
             <>
             <div className="search">
@@ -41,9 +53,11 @@ export default function Weather() {
             <div className={show? "weather" : "weather loading"}>
                 <h1 className="city">Weather in {result?.name}</h1>
                 <div className="temp">{Math.round(result?.main?.temp)}°C</div>
-1               <div className="feels_like">Feels like {Math.round(result?.main?.feels_like)}°C</div>
-                <img src="" alt="" className="icon"/>
-                <div className="description">{result?.weather?.description}</div>
+               <div className="feels_like">Feels like {Math.round(result?.main?.feels_like)}°C</div>
+               <img 
+                    src={WeatherIcons[iconFilename]} 
+                    alt="Icon" />
+                <div className="description">{description}</div>
                 <div className="div-table">
                     <div className="div-row">
                         <div className="humidity">Humidity: {result?.main?.humidity}%</div>
@@ -70,8 +84,8 @@ export default function Weather() {
                 </p>
                 <div className="collapse" id="collapseExample">
                     <div className="card card-body">
-                    <div className="sunrise">Sunrise time: {result?.sys?.sunrise}</div>
-                    <div className="sunset">Sunset time: {result?.sys?.sunset}</div>
+                    <div className="sunrise">Sunrise time: {sunriseTime}</div>
+                    <div className="sunset">Sunset time: {sunsetTime}</div>
                     <div className="date">{result?.timezone}</div>
                     </div>
                 </div>
