1d392fa3a11dc438a0e5654de7b0ba085d33c8e8
Masoud Nazarii <masoudnazari.dev@gmail.com>
2023-07-11 22:07:57 +0330

    import theme styles

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,7 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.css';
-// import './theme.js';
-// import './styles/theme.css'
 import './styles/App.css';
+import './styles/theme.css'
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import Navbar from './Components/Navbar';
 import MainCard from './Components/MainCard';
