17c9f9b0154e3b648a63c799090c6c4bd2486f05
Masoud Nazarii <masoudnazari.dev@gmail.com>
2023-07-11 22:08:21 +0330

    add theme code

--- weather/src/Components/MainCard.js
+++ weather/src/Components/MainCard.js
@@ -1,16 +1,40 @@
-import React from "react";
-import Theme from "./Theme";
+import React,{useState, createContext} from "react";
 // import Search from "./Search";
+// import { ThemeContext } from "./Theme";
 import Weather from "./Weather";
+import { Sun, Moon } from "./Icons";
+
+export const ThemeContext = createContext();
 
 export default function MainCard() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+    const handleThemeChange = () => {
+        setIsDarkMode(!isDarkMode);
+    };
+  
   return (
     <>
-      <div className="main-card">
-        <Theme />
-        {/* <Search /> */}
-        <Weather />
-      </div>
+      <ThemeContext.Provider value={isDarkMode}>
+        <div className={isDarkMode ? "main-card main-card-dark" : "main-card"}>
+          <div>
+              <input type="checkbox" 
+                  className="checkbox" 
+                  id="chk"
+                  checked={isDarkMode}
+                  onChange={handleThemeChange} />
+              <label 
+                  className={isDarkMode ? "label-dark label" : "label"} 
+                  htmlFor="chk">
+                  <Sun />
+                  <Moon />
+                  <div className="ball" />
+              </label>
+          </div>
+          {/* <Search /> */}
+          <Weather />
+        </div>
+      </ThemeContext.Provider>
     </>
   );
 }
