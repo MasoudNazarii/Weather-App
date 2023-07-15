c220a5a611987ef46382a51812868d5f1f8acd18
Masoud Nazarii <masoudnazari.dev@gmail.com>
2023-07-15 22:58:00 +0330

    test

--- weather/src/icons/weather-icons-master/production/line/openweathermap/index.js
+++ weather/src/icons/weather-icons-master/production/line/openweathermap/index.js
@@ -1,24 +1,25 @@
 
+
+
 const WeatherIcons = {
-    '01d': './01d.svg',
-    '01n': './01n.svg',
-    '02d': './02d.svg',
-    '02n': './02n.svg',
-    '03d': './03d.svg',
-    '03n': './03n.svg',
-    '04d': './04d.svg',
-    '04n': './04n.svg',
-    '09d': './09d.svg',
-    '09n': './09n.svg',
-    '10d': './10d.svg',
-    '10n': './10n.svg',
-    '11d': './11d.svg',
-    '11n': './11n.svg',
-    '13d': './13d.svg',
-    '13n': './13n.svg',
-    '50d': './50d.svg',
-    '50n': './50n.svg'
-  };
+    d10: require('./01d.svg'),
+    n10: require('./01n.svg'),
+    d20: require('./02d.svg'),
+    n20: require('./02n.svg'),
+    d30: require('./03d.svg'),
+    n30: require('./03n.svg'),
+    d40: require('./04d.svg'),
+    n40: require('./04n.svg'),
+    d90: require('./09d.svg'),
+    n90: require('./09n.svg'),
+    d01: require('./10d.svg'),
+    n01: require('./10n.svg'),
+    d11: require('./11d.svg'),
+    n11: require('./11n.svg'),
+    d31: require('./13d.svg'),
+    n31: require('./13n.svg'),
+    d05: require('./50d.svg'),
+    n05: require('./50n.svg'),
+}
   
   export default WeatherIcons;
-
