32cde74e114fc0657fdfb65673327ca4892ecd71
Masoud <masoudnazari.dev@gmail.com>
2024-02-08 13:48:36 +0330

    add 3D earth madel

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import './styles/theme.css'
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import Navbar from './Components/Navbar';
 import MainCard from './Components/MainCard';
+import Spline from '@splinetool/react-spline';
 
 function App() {
   return (
     <>
 		<Navbar />
+    <iframe className='Earth' src='https://my.spline.design/untitled-acb7e6c4af18e696bb86444ce0386952/' frameborder='0' width='30%' height='30%'></iframe>
+    
 		<MainCard />
     </>
   );
